191a3d318dd34fa4c3d2555a7335494a1f09ee57
Matt Cosand <matt@cosand.net>
2020-12-08 21:44:59 -0800

    Day 2 part 1

---
+++ day-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace day_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[][] lines = System.IO.File.ReadAllLines("input.txt").Select(f => f.Split(' ')).ToArray();
+
+      int count = lines.Select(f =>
+      {
+        int c = f[2].Where(g => g == f[1][0]).Count();
+        int[] range = f[0].Split('-').Select(g => int.Parse(g)).ToArray();
+        return c >= range[0] && c <= range[1] ? 1 : 0;
+      }).Sum();
+
+      Console.WriteLine(count);
+
+      }
+  }
+}
