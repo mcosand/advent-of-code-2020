fae2f0606edaca162d837a9a2cd1af63d4999597
Matt Cosand <matt@cosand.net>
2020-12-13 16:12:13 -0800

    Day 3 part 1

---
+++ day-03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace day_03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int count = System.IO.File.ReadAllLines("input.txt")
+        .Skip(1)
+        .Select((line, idx) =>
+        {
+          bool isTree = line[((idx + 1) * 3) % line.Length] == '#';
+          Console.WriteLine(isTree);
+          return isTree ? 1 : 0;
+        })
+    .Sum();
+    }
+  }
+}
